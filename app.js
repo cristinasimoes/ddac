62b032276c5030c65b22d2812fee5d57488c5ae5
Cristina Pinto Simões <cristinapinto.simoes@gmail.com>
2021-06-13 23:49:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ var subscribe2 = document.getElementById('subscribeBtn2');
 var subscribeTitle = document.querySelector('.subscribe');
 
 var image = document.getElementById('image');
-var wrapper2 = document.getElementById('wrapper2');
+var wrapper2 = document.getElementById('content');
 var title = document.querySelector('.title');
 var container2 = document.querySelector('.container2');
 var image2 = document.getElementById('image2');
 var email = document.getElementById('email');
+var body = document.getElementsByTagName('body');
 
 var formContainer = document.getElementById('formContainer');
-var container = document.getElementById('container');
+var container = document.getElementById('containerTitle');
+
+
+
 subscribe.addEventListener('click', function(){
-    console.log(1);
-    image.style.display= "none";
-    wrapper2.style.bottom ="-88vh";
+    document.body.style.backgroundImage ="none";
+    // wrapper2.style.bottom ="-88vh";
     title.style.display= "none";
     container2.style.display= "none";
     image2.style.display= "block";
@@ -22,15 +25,13 @@ subscribe.addEventListener('click', function(){
     subscribe.style.display="none";
     subscribe2.style.display="block";
     subscribeTitle.innerHTML = "Thanks for your subscription, you will hear from us!";
-   
     subscribeTitle.style.width="285px" ;
-     // formContainer.style.paddingBottom ="74px";
     container.style.paddingBottom ="66px";
-
+    container.style.marginTop ="-163px";
 });
 subscribe2.addEventListener('click', function(){
-    image.style.display= "block";
-    wrapper2.style.bottom ="12vh";
+    document.body.style.backgroundImage ="url('pic.png')";
+    // wrapper2.style.bottom ="12vh";
     title.style.display= "block";
     container2.style.display= "flex";
     image2.style.display= "none";
@@ -39,10 +40,8 @@ subscribe2.addEventListener('click', function(){
     subscribe2.style.display="none";
     subscribeTitle.innerHTML = "Subscribe to get more information";
     subscribeTitle.style.width="194px" ;
-
+    container.style.marginTop ="0";
     // formContainer.style.paddingBottom ="0";
     container.style.paddingBottom ="100px";
-
-
 });
 
