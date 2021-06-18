a06202fbdc792cdd6f31a513498ac62de0b554b5
Cristina Pinto Simões <cristinapinto.simoes@gmail.com>
2021-06-19 00:11:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,41 +4,37 @@ var subscribeTitle = document.querySelector('.subscribe');
 
 var image = document.getElementById('image');
 var wrapper2 = document.getElementById('content');
-var title = document.querySelector('.title');
-var container2 = document.querySelector('.container2');
-var image2 = document.getElementById('image2');
+var image = document.getElementById('image');
 var email = document.getElementById('email');
 var body = document.getElementsByTagName('body');
 
 var formContainer = document.getElementById('formContainer');
 var container = document.getElementById('containerTitle');
+var content = document.getElementById('content');
 
 
 
 subscribe.addEventListener('click', function(){
     document.body.style.backgroundImage ="none";
-    title.style.display= "none";
-    container2.style.display= "none";
-    image2.style.display= "block";
+    image.src = "media/gif.gif"
     email.style.display="none";
     subscribe.style.display="none";
     subscribe2.style.display="block";
     subscribeTitle.innerHTML = "Thanks for your subscription, you will hear from us!";
-    subscribeTitle.style.width="285px" ;
-    container.style.paddingBottom ="66px";
-    container.style.marginTop ="-163px";
+    content.style.top="57%";
+
+    // container.style.paddingBottom ="66px";
+    // container.style.marginTop ="-163px";
 });
 subscribe2.addEventListener('click', function(){
     document.body.style.backgroundImage ="url('media/bg-image.png')";
-    title.style.display= "block";
-    container2.style.display= "flex";
-    image2.style.display= "none";
+    image.src = "media/title.png"
     email.style.display="block";
     subscribe.style.display= "block";
     subscribe2.style.display="none";
     subscribeTitle.innerHTML = "Subscribe to get more information";
-    subscribeTitle.style.width="194px" ;
-    container.style.marginTop ="0";
-    container.style.paddingBottom ="100px";
+    content.style.top="65%";
+    // container.style.marginTop ="0";
+    // container.style.paddingBottom ="100px";
 });
 
